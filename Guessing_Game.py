1b3fe6ab1272206b8321978b3546d1506ae19f40
JMori7694 <41706211+JMori7694@users.noreply.github.com>
2023-02-06 21:27:50 -0500

    Create Guessing_Game.py
    Add a basic description of what is planned to be accomplished as well as first line of code

---
+++ Guessing_Game.py
@@ -0,0 +1,8 @@
+# Random Number Generator Guessing Game
+# Three levels of difficulty
+# Easy: 1-10
+# Medium: 1-50
+# Hard: 1-100
+# Wins and loses for each level will be put into lists to display upon ending the game
+
+import random
