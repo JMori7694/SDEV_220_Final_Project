9fc44fa5efcd47bb7e5c4b3ebc45c2158178a20f
Sarah Lesher <seal21295@outlook.com>
2023-03-06 20:43:00 -0500

    Update Guessing_Gamev2.py

--- Guessing_Gamev2.py
+++ Guessing_Gamev2.py
@@ -6,233 +6,84 @@
 # Wins and loses for each level will be put into lists to display upon ending the game
 
 import random
-<<<<<<< Updated upstream
-=======
-# Medium 1-50
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+# tkinter
+window = tk.Tk()
+window.title('Number Guessing Game')
+
+window.rowconfigure([0, 1, 2, 3, 4, 5, 6, 7, 8], minsize=50, weight=1)
+window.columnconfigure([0, 1, 2, 3, 4, 5, 6, 7, 8], minsize=50, weight=1)
+
+lbl = tk.Label(text='Would you like to play a number guessing game?')
+lbl.grid(row=0, columnspan=9)
+
+btn_y = tk.Button(
+    master=window,
+    text='Yes',
+)
+btn_y.grid(row=1, column=2)
+
+btn_n = tk.Button(
+    text='No',
+)
+btn_n.grid(row=1, column=6)
+
+window.mainloop()
 
 
-
-games_medium = []
-wins_medium = 0
-loss_medium = 0
-def show_score_medium():
-    global wins_medium
-    global loss_medium
-
-    if not wins_medium and loss_medium:
-        print('No games have been played yet.')
-
-    elif not wins_medium:
-        print(f'You have lost {loss_medium} game(s) out of {games_medium}game(s).')
-
-    elif not loss_medium:
-        print(f'You have won {wins_medium} game(s) out of {max(games_medium)} game(s)')
-    else:
-        print(f'You have played {max(games_medium)} games with {wins_medium} win(s) and {loss_medium}loss(es).')
-    if not wins_medium and loss_medium:
-        print('No games have been played yet.')
-
-    elif not wins_medium:
-        print(f'You have lost {loss_medium}  game(s) out of {games_medium} game(s).')
-
-    elif not loss_medium:
-        print(f'You have won {wins_medium} game(s) out of {max(games_medium)}  game(s)')
-
-    else:
-        print(f'You have played ' + {max(games_medium)} + ' games with ' +
-              wins_medium + ' win(s) and ' + loss_medium + ' loss(es).')
-
-def start_medium():
-    global wins_medium
-    global loss_medium
-    guesses_medium = 0
-    rand_num = random.randint(1, 50)
-    wanna_play = input('Are you ready to play the medium level guessing game?    (Enter Yes/No): ')
-    print("You have 5 guesses")
-
-    if wanna_play.lower() != 'yes':
-        print('Maybe next time!')
-        exit()
-    else:
-        show_score_medium()
-
-    while wanna_play.lower() == 'yes':
-        if guesses_medium == 5: 
-            loss_medium += 1 
-
-            print("You've run out of guesses!")
-            break
-        try:
-            guess = int(input('Pick a number between 1 and 50: '))
-            if guess < 1 or guess > 50:
-                raise ValueError(
-                    'Please guess a number between 1 and 50')
-
-            guesses_medium += 1
-
-            if guess == rand_num:
-                wins_medium += 1 
-                print('Nice! You got it!')
-                print(f'It took you {guesses_medium} attempts')
-                wanna_play = input(
-                    'Would you like to play again? (Enter Yes/No): ')
-                if wanna_play.lower() != 'yes':
-                    print('Later!')
-                    break
-                else:
-                    guesses_medium = 0
-                    rand_num = random.randint(1, 50)
-                    show_score_easy()
-                    continue
-            else:
-                if guess > rand_num:
-                    print('It\'s lower')
-                elif guess < rand_num:
-                    print('It\'s higher')
-
-        except ValueError as err:
-            print('Oh no!, that is not a valid value. Try again...')
-            print(err)
-
-def show_score_hard():
-    global wins_hard
-    global loss_hard
-
-    if not wins_hard and loss_hard:
-        print('No games have been played yet.')
-
-    elif not wins_hard:
-        print(f'You have lost'+ loss_hard +
-             'game(s) out of'+ games_hard +'game(s).')
-
-    elif not loss_hard:
-        print(f'You have won'+ wins_hard +
-             'game(s) out of'+ {max(games_hard)} +'game(s)')
-
-    else:
-        print(f'You have played'+ {max(games_hard)} +'games with'+
-              wins_hard +'win(s) and'+ loss_hard +'loss(es).')
-    if not wins_hard and loss_hard:
-        print('No games have been played yet.')
-
-    elif not wins_hard:
-        print(f'You have lost ' + loss_hard +
-              ' game(s) out of ' + games_hard + ' game(s).')
-
-    elif not loss_hard:
-        print(f'You have won ' + wins_hard +
-              ' game(s) out of ' + {max(games_hard)} + ' game(s)')
-
-    else:
-        print(f'You have played ' + {max(games_hard)} + ' games with ' +
-              wins_hard + ' win(s) and ' + loss_hard + ' loss(es).')
-
-
-def start_hard():
-    guesses_hard = 0
-    global loss_hard
-    global wins_hard
-    rand_num = random.randint(1, 100)
-    wanna_play= input(
-        f'Are you ready to play the medium level guessing game?'
-        '(Enter Yes/No): ')
-    print("You have 10 guesses") 
-
-    if wanna_play.lower() != 'yes':
-        print('Maybe next time!')
-        exit()
-    else:
-        show_score_hard()
-
-    while wanna_play.lower() == 'yes':
-        if guesses_hard == 10: 
-            loss_hard += 1 
-
-            print("You've run out of guesses!")
-            break
-        try:
-            guess = int(input('Pick a number between 1 and 100: '))
-            if guess < 1 or guess > 100:
-                raise ValueError(
-                    'Please guess a number between 1 and 100')
-
-            guesses_hard += 1
-
-            if guess == rand_num:
-                wins_hard += 1 
-                print('Nice! You got it!')
-                print(f'It took you {guesses_hard} attempts')
-                wanna_play = input(
-                    'Would you like to play again? (Enter Yes/No): ')
-                if wanna_play.lower() != 'yes':
-                    print('Later!')
-                    break
-                else:
-                    guesses_hard = 0
-                    rand_num = random.randint(1, 100)
-                    show_score_easy()
-                    continue
-            else:
-                if guess > rand_num:
-                    print('It\'s lower')
-                elif guess < rand_num:
-                    print('It\'s higher')
-
-        except ValueError as err:
-            print('Oh no!, that is not a valid value. Try again...')
-            print(err)
-            
 def start_game():
     play = input(
-        'Would you like to play a number guessing game? (Enter yes or no)')
+        'Would you like to play a number guessing game? (Enter yes or no) ')
     if play.lower() == 'yes':
-        difficulty = input('Please enter Easy, Medium or Hard.')
+        difficulty = input('Please enter Easy, Medium or Hard. ')
         if difficulty.lower() == 'easy':
             start_easy()
-        elif difficulty.lower() == 'medium':            
+        elif difficulty.lower() == 'medium':
             start_medium()
         elif difficulty.lower() == 'hard':
             start_hard()
         else:
-            print('Please enter Easy, Medium, or Difficult to continue.')
+            print('Please enter Easy, Medium, or Hard to continue. ')
     else:
         print('Maybe next time!')
 
 # Easy: 1-10,
 
 
-games_easy = []
+a = 0
+games_easy = [a]
 wins_easy = 0
 loss_easy = 0
 
 
-def show_score_easy():
-    global wins_easy
-    global loss_easy
-    if not wins_easy and loss_easy:
-        print('No games have been played yet.')
-
-    elif not wins_easy:
-        print(f"You have lost {loss_easy} game(s) out of  {games_easy}   game(s).")
-
-    elif not loss_easy:
-        print(f'You have won ' + wins_easy +
-              ' game(s) out of ' + {max(games_easy)} + ' game(s)')
-
-    else:
-        print(f'You have played ' + {max(games_easy)} + ' games with ' +
-              wins_easy + ' win(s) and ' + loss_easy + ' loss(es).')
-
-
 def start_easy():
-    global wins_easy
-    global loss_easy
+    global a, games_easy, wins_easy, loss_easy
     guesses_easy = 0
     rand_num = random.randint(1, 10)
     wanna_play = input(
         f'Are you ready to play the Easy level guessing game?'
         '(Enter Yes/No): ')
     print("You have 3 guesses")
+
+    def show_score_easy():
+        if wins_easy == 0 and loss_easy == 0:
+            print('No games have been played yet.')
+
+        elif wins_easy == 0 and loss_easy >= 1:
+            print(
+                f'You have lost ' + str(loss_easy) +
+                ' game(s) out of ' + str(max(games_easy)) + ' game(s).')
+
+        elif loss_easy == 0 and wins_easy >= 1:
+            print(f'You have won ' + str(wins_easy) +
+                  ' game(s) out of ' + str(max(games_easy)) + ' game(s)')
+
+        else:
+            print(f'You have played ' + str(max(games_easy)) + ' games with ' +
+                  str(wins_easy) + ' win(s) and ' + str(loss_easy) + ' loss(es).')
 
     if wanna_play.lower() != 'yes':
         print('Maybe next time!')
@@ -242,8 +93,11 @@ def start_easy():
 
     while wanna_play.lower() == 'yes':
         if guesses_easy == 3:
+            a += 1
+            loss_easy += 1
+            games_easy.append(a)
             print("You've run out of guesses!")
-            break
+            start_game()
         try:
             guess = int(input('Pick a number between 1 and 10: '))
             if guess < 1 or guess > 10:
@@ -253,9 +107,11 @@ def start_easy():
             guesses_easy += 1
 
             if guess == rand_num:
-                loss_easy += 1 
+                a += 1
+                wins_easy += 1
+                games_easy.append(a)
                 print('Nice! You got it!')
-                print(f'It took you {guesses_easy} attempts')
+                print(f'It took you ' + str(guesses_easy) + ' attempts.')
                 wanna_play = input(
                     'Would you like to play again? (Enter Yes/No): ')
                 if wanna_play.lower() != 'yes':
@@ -276,108 +132,78 @@ def start_easy():
             print('Oh no!, that is not a valid value. Try again...')
             print(err)
 
-
-if __name__ == '__main__':
-    start_game()
-
-
 # Medium 1-50
 
 
-
-games_medium = []
+b = 0
+games_medium = [b]
 wins_medium = 0
 loss_medium = 0
 
 
+def start_medium():
+    global b, games_medium, wins_medium, loss_medium
+    guesses_medium = 0
+    rand_num = random.randint(1, 50)
+    wanna_play = input(
+        'Are you ready to play the medium level guessing game?    (Enter Yes/No): ')
+    print("You have 5 guesses")
 
+    def show_score_medium():
+        if wins_medium == 0 and loss_medium == 0:
+            print('No games have been played yet.')
 
+        elif wins_medium == 0 and loss_medium >= 1:
+            print(
+                f'You have lost ' + str(loss_medium) +
+                ' game(s) out of ' + str(max(games_medium)) + ' game(s).')
 
-
-
-# Hard 1-100
-games_hard = []
-wins_hard = 0
-loss_hard = 0
-
-
-def show_score_hard():
-    global wins_hard
-    global loss_hard
-
-    if not wins_hard and loss_hard:
-        print('No games have been played yet.')
-
-    elif not wins_hard:
-        print(f'You have lost'+ loss_hard +
-             'game(s) out of'+ games_hard +'game(s).')
-
-    elif not loss_hard:
-        print(f'You have won'+ wins_hard +
-             'game(s) out of'+ {max(games_hard)} +'game(s)')
-
-    else:
-        print(f'You have played'+ {max(games_hard)} +'games with'+
-              wins_hard +'win(s) and'+ loss_hard +'loss(es).')
-    if not wins_hard and loss_hard:
-        print('No games have been played yet.')
-
-    elif not wins_hard:
-        print(f'You have lost ' + loss_hard +
-              ' game(s) out of ' + games_hard + ' game(s).')
-
-    elif not loss_hard:
-        print(f'You have won ' + wins_hard +
-              ' game(s) out of ' + {max(games_hard)} + ' game(s)')
-
-    else:
-        print(f'You have played ' + {max(games_hard)} + ' games with ' +
-              wins_hard + ' win(s) and ' + loss_hard + ' loss(es).')
-
-
-def start_hard():
-    guesses_hard = 0
-    global loss_hard
-    global wins_hard
-    rand_num = random.randint(1, 100)
-    wanna_play= input(
-        f'Are you ready to play the medium level guessing game?'
-        '(Enter Yes/No): ')
-    print("You have 10 guesses") 
+        elif loss_medium == 0 and wins_medium >= 1:
+            print(
+                f'You have won ' + str(wins_medium) +
+                ' game(s) out of ' + str(max(games_medium)) + ' game(s)')
+        else:
+            print(
+                f'You have played ' + str(max(games_medium)) +
+                ' games with ' + str(wins_medium) + ' win(s) and '
+                + str(loss_medium) + ' loss(es).')
 
     if wanna_play.lower() != 'yes':
         print('Maybe next time!')
         exit()
     else:
-        show_score_hard()
+        show_score_medium()
 
     while wanna_play.lower() == 'yes':
-        if guesses_hard == 10: 
-            loss_hard += 1 
-
+        if guesses_medium == 5:
+            b += 1
+            loss_medium += 1
+            games_medium.append(b)
             print("You've run out of guesses!")
-            break
+            start_game()
         try:
-            guess = int(input('Pick a number between 1 and 100: '))
-            if guess < 1 or guess > 100:
+            guess = int(input('Pick a number between 1 and 50: '))
+            if guess < 1 or guess > 50:
                 raise ValueError(
-                    'Please guess a number between 1 and 100')
+                    'Please guess a number between 1 and 50')
 
-            guesses_hard += 1
+            guesses_medium += 1
 
             if guess == rand_num:
-                wins_hard += 1 
+                b += 1
+                wins_medium += 1
+                games_medium.append(b)
                 print('Nice! You got it!')
-                print(f'It took you {guesses_hard} attempts')
+                print(f'It took you ' + str(guesses_medium) + ' attempts.')
                 wanna_play = input(
                     'Would you like to play again? (Enter Yes/No): ')
                 if wanna_play.lower() != 'yes':
                     print('Later!')
                     break
                 else:
-                    guesses_hard = 0
-                    rand_num = random.randint(1, 100)
-                    show_score_easy()
+                    guesses_medium = 0
+                    rand_num = random.randint(1, 50)
+                    show_score_medium()
                     continue
             else:
                 if guess > rand_num:
@@ -388,4 +214,99 @@ def start_hard():
         except ValueError as err:
             print('Oh no!, that is not a valid value. Try again...')
             print(err)
->>>>>>> Stashed changes
+
+# Hard 1-100
+
+
+c = 0
+games_hard = [c]
+wins_hard = 0
+loss_hard = 0
+
+
+def start_hard():
+    global c, games_hard, wins_hard, loss_hard
+    guesses_hard = 0
+    rand_num = random.randint(1, 100)
+    wanna_play = input(
+        f'Are you ready to play the Hard level guessing game?'
+        '(Enter Yes/No): ')
+    print("You have 10 guesses")
+
+    def show_score_hard():
+        if wins_hard == 0 and loss_hard == 0:
+            print('No games have been played yet.')
+
+        elif wins_hard == 0 and loss_hard >= 0:
+            print(f'You have lost ' + str(loss_hard) +
+                  ' game(s) out of ' + str(max(games_hard)) + ' game(s).')
+
+        elif loss_hard == 0 and wins_hard >= 1:
+            print(f'You have won ' + str(wins_hard) +
+                  ' game(s) out of ' + str(max(games_hard)) + ' game(s)')
+
+        else:
+            print(f'You have played ' + str(max(games_hard)) + ' games with ' +
+                  str(wins_hard) + ' win(s) and ' + str(loss_hard) + ' loss(es).')
+
+    if wanna_play.lower() != 'yes':
+        print('Maybe next time!')
+        exit()
+    else:
+        show_score_hard()
+
+    while wanna_play.lower() == 'yes':
+        if guesses_hard == 10:
+            c += 1
+            loss_hard += 1
+            games_hard.append(c)
+            print("You've run out of guesses!")
+            start_game()
+        try:
+            guess = int(input('Pick a number between 1 and 100: '))
+            if guess < 1 or guess > 100:
+                raise ValueError(
+                    'Please guess a number between 1 and 100')
+
+            guesses_hard += 1
+
+            if guess == rand_num:
+                c += 1
+                wins_hard += 1
+                games_hard.append(c)
+                print('Nice! You got it!')
+                print(f'It took you ' + str(guesses_hard) + ' attempts.')
+                wanna_play = input(
+                    'Would you like to play again? (Enter Yes/No): ')
+                if wanna_play.lower() != 'yes':
+                    print('Later!')
+                    break
+                else:
+                    guesses_hard = 0
+                    rand_num = random.randint(1, 100)
+                    show_score_hard()
+                    continue
+            else:
+                if guess > rand_num:
+                    print('It\'s lower')
+                elif guess < rand_num:
+                    print('It\'s higher')
+
+        except ValueError as err:
+            print('Oh no!, that is not a valid value. Try again...')
+            print(err)
+
+
+if __name__ == '__main__':
+    start_game()
+
+
+# classes
+level = ['Easy', 'Medium', 'Hard']
+
+
+class level:
+    def __init__(level_difficulty):
+        level.hard = loss_hard
+        level.medium = loss_medium
+        level.easy = loss_easy
